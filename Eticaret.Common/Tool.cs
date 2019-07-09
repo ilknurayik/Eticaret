aa3505b9cb8e0e507a91006f270a861b450c9ee8
ilknur.ayik <ilknur.ayik35@gmail.com>
2019-07-09 21:12:55 +0300

    Entity model eklendi, db bağlantısı yapıldı

---
+++ Eticaret.Common/Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eticaret.Entity;
+
+namespace Eticaret.Common
+{
+    public class Tool
+    {
+        public static ECommerceEntities db = null;
+        public static ECommerceEntities GetConnection()
+        {
+            if (db is null)
+            {
+                db = new ECommerceEntities();
+            }
+
+            return db;
+        }
+    }
+}
